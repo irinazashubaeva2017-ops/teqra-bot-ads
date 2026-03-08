4580a3470f8761d0bcb508cadeb52c1dc0439a0d
irinazashubaeva2017-ops <irinazashubaeva2017@gmail.com>
2026-03-08 17:43:02 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 import sqlite3
 
 def init_db():
-    # Создаем файл базы данных
     conn = sqlite3.connect('teqra_data.db')
     cursor = conn.cursor()
-    # Создаем таблицу пользователей, если её нет
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY,
@@ -17,7 +15,6 @@ def init_db():
 def add_user(user_id, username):
     conn = sqlite3.connect('teqra_data.db')
     cursor = conn.cursor()
-    # Добавляем пользователя (ignore — чтобы не было ошибок при повторном входе)
     cursor.execute('INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)', (user_id, username))
     conn.commit()
     conn.close()
